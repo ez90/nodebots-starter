8c4edd65ca498b7b163aeca588ae7d9cbed5e348
Julien Gonzalez <machette@hotmail.fr>
2018-10-15 23:01:34 +0200

    Clean code

--- src/client/index.js
+++ src/client/index.js
@@ -1,55 +1,23 @@
 import 'babel-polyfill';
 import socketIOClient from 'socket.io-client';
 
-// Set socketIO to the window location given by the server
+// Set socketIO to the server
 const io = socketIOClient('http://localhost:8090/');
 
 // Get the button for the LED toggle in the DOM
 const ledToggle = document.getElementById('led-toggle');
 
 // Set browser-side lightOn variable as true by default
-let lightOn = true;
+let ledActive = true;
 
-/**
- *
- * Client-Side Socket Events
- *
- * On connection, tell the server that the client side has connected.
- * Upon connection, a 'light-state' event may be sent from the server.
- * Set the lightOn variable to the current state on the server side.
- *
- */
-io.on('connection', () => {
-  console.log('[socket.io] Client has Connected!');
-
-  // Get robot's current light state from the server side.
-  io.on('light-state', (lightState) => {
-    console.log(`[socket.io] Light is currently ${lightState ? 'on' : 'off'}.`);
-    lightOn = lightState;
-    if (ledToggle && !lightOn) {
-      ledToggle.innerHTML = 'Turn On LED';
-    }
-  });
-});
-
-/**
- *
- * If the LED toggle button is found in the DOM, check when it's clicked.
- * When clicked, update the lightOn boolean, then send the value down to the
- * server and update the robot's LED state.
- *
- */
 if (ledToggle) {
   ledToggle.addEventListener('click', () => {
-    if (lightOn) {
-      lightOn = false;
-      ledToggle.innerHTML = 'Turn On LED';
-    } else {
-      lightOn = true;
-      ledToggle.innerHTML = 'Turn Off LED';
-    }
+    ledActive = !ledActive;
 
-    // Send new value down to the server.
-    io.emit('light-toggle', lightOn);
+    ledToggle.innerHTML = (ledActive)
+      ? 'Turn Off LED'
+      : 'Turn On LED';
+
+    io.emit('toggle-led');
   });
 }
