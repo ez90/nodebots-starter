80f1adc3ab24b7fd5bebdc5eb3088a4c9464af0b
Julien Gonzalez <machette@hotmail.fr>
2018-10-27 17:19:33 +0200

    Quick test for ansible

--- src/server/index.js
+++ src/server/index.js
@@ -1,17 +1,10 @@
-import { Board, Led } from 'johnny-five';
-import express from 'express';
-import http from 'http';
-import SocketIO from 'socket.io';
-
+const express = require('express');
 const app = express();
-const server = http.Server(app);
-const io = new SocketIO(server);
-const port = 8090;
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-server.listen(port, () => {
-  console.log('[express] Listening on *:' + port);
+app.listen(8090, function () {
+  console.log('Example app listening on port 8090!');
 });
