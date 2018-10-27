4b821935dd0d88def7797ec2216849f8717829e7
Julien Gonzalez <machette@hotmail.fr>
2018-10-27 15:57:06 +0200

    Quick test for ansible

---
+++ src/server/index-save.js
@@ -0,0 +1,62 @@
+import { Board, Led } from 'johnny-five';
+import express from 'express';
+import http from 'http';
+import SocketIO from 'socket.io';
+
+const app = express();
+const server = http.Server(app);
+const io = new SocketIO(server);
+const port = 8090;
+
+// app.use(express.static('dist'));
+
+// app.get('/', (req, res) => {
+//   res.sendFile(path.resolve('dist/index.html'));
+// });
+
+server.listen(port, () => {
+  console.log('[express] Listening on *:' + port);
+});
+
+// Init state
+let ledActive = true;
+
+/**
+ * Turn led on/off
+ * @param led Led
+ * @param status boolean
+ */
+const toggleLed = (led, status) => {
+  if (status) {
+    led.on();
+  } else {
+    led.off();
+  }
+};
+
+const board = Board();
+board.on('ready', function () {
+
+  // Led instance connected on Arduino port 13
+  const led = new Led(13);
+
+  // This will grant access to the led instance
+  // from within the REPL that's created when
+  // running this program.
+  this.repl.inject({
+    led: led,
+  });
+
+  toggleLed(led, ledActive);
+
+  io.on('connect', (socket) => {
+    console.log('[socket.io] Client has connected to server');
+
+    socket.on('toggle-led', () => {
+      console.log('[socket.io] toggle-led emitted');
+
+      ledActive = !ledActive;
+      toggleLed(led, ledActive);
+    });
+  });
+});
