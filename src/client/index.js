a5bd3a6894b98040cdf59f70411eddf5a16f1ac0
Julien Gonzalez <machette@hotmail.fr>
2018-11-05 23:53:52 +0100

    Add temperature feature and sass

--- src/client/index.js
+++ src/client/index.js
@@ -1,13 +1,15 @@
 import 'babel-polyfill';
 import socketIOClient from 'socket.io-client';
 
+import './scss/app.scss';
+
 // Set socketIO to the server
 const io = socketIOClient('http://localhost:8090');
 
-// Get the button for the LED toggle in the DOM
+// UI dynamic elements
 const ledToggle = document.getElementById('led-toggle');
+const temperature = document.getElementById('temperature');
 
-// Set browser-side lightOn variable as true by default
 let ledActive = true;
 
 if (ledToggle) {
@@ -19,5 +21,13 @@ if (ledToggle) {
       : 'Turn On LED';
 
     io.emit('toggle-led');
+  });
+}
+
+if (temperature) {
+  io.on('temperature-reading', function (message) {
+    temperature.innerHTML = parseInt(message);
+    let hue = 200 - (parseInt(message) * 5);
+    document.body.style.backgroundColor = "hsl(" + hue + ", 60%, 65%)";
   });
 }
