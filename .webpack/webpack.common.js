cd3257975c93a34a9153f71e101a6e16f00b815a
Julien Gonzalez <machette@hotmail.fr>
2018-11-05 23:52:37 +0100

    Update webpack directories structure

--- .webpack/webpack.common.js
+++ .webpack/webpack.common.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const outputDirectory = 'dist';
 
@@ -8,8 +9,28 @@ module.exports = {
   entry: [
     './src/client/index.js',
   ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
   plugins: [
     new CleanWebpackPlugin([outputDirectory]),
+    new MiniCssExtractPlugin({
+      filename: 'app.css',
+    }),
     new HtmlWebpackPlugin({
       template: './public/index.html',
       inject: true,
